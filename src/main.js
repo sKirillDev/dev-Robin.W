54d77814eb1cfca825b21ca52b97347c610e56d7
sKirillDev <k.sychov.dp@gmail.com>
2025-10-30 12:47:54 +0200

    feat: tabs done

--- src/main.js
+++ src/main.js
@@ -8,3 +8,31 @@ burgerBtn.addEventListener('click', ()=> {
     burgerBtn.classList.toggle('after:bg-purple')
     burgerBtn.classList.toggle('before:bg-purple')
 })
+
+const installTabs =()=> {
+    const tabsButtons = document.querySelectorAll('[data-tab-content]');
+    const tabContent = document.querySelectorAll('.tab-content')
+
+    const removeActiveClassForNav =()=> {
+        tabsButtons.forEach(item => item.classList.remove('bg-purple'))
+    }
+    
+    const hiddenContentTabs =()=> {
+        tabContent.forEach(item => item.classList.add('hidden'))
+    }
+
+    tabsButtons.forEach((item, index, array)=>{
+        const tabsButton = item.getAttribute('data-tab-content');
+        
+        item.addEventListener('click', ()=>{
+            removeActiveClassForNav()
+            item.classList.add('bg-purple')
+
+            const showContent = document.getElementById(tabsButton);
+            hiddenContentTabs()
+            showContent.classList.remove('hidden')
+        })
+    })
+}
+
+document.querySelectorAll('.tabs').length ? installTabs() :null;
