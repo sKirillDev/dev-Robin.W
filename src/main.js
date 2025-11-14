76f1471c8e505812acf04843ea080ecf97124966
sKirillDev <k.sychov.dp@gmail.com>
2025-11-14 11:19:51 +0200

    feat: partners slider done

--- src/main.js
+++ src/main.js
@@ -1,38 +1,74 @@
 import './style.css'
 
-const burgerBtn = document.querySelector('#burger')
-const mobileContainer = document.querySelector('#mobile-container')
+window.addEventListener('load', () => {
 
-burgerBtn.addEventListener('click', ()=> {
-    mobileContainer.classList.toggle('hidden')
-    burgerBtn.classList.toggle('after:bg-purple')
-    burgerBtn.classList.toggle('before:bg-purple')
-})
+    const burgerBtn = document.querySelector('#burger')
+    const mobileContainer = document.querySelector('#mobile-container')
 
-const installTabs =()=> {
-    const tabsButtons = document.querySelectorAll('[data-tab-content]');
-    const tabContent = document.querySelectorAll('.tab-content')
-
-    const removeActiveClassForNav =()=> {
-        tabsButtons.forEach(item => item.classList.remove('bg-purple'))
-    }
-    
-    const hiddenContentTabs =()=> {
-        tabContent.forEach(item => item.classList.add('hidden'))
-    }
-
-    tabsButtons.forEach((item, index, array)=>{
-        const tabsButton = item.getAttribute('data-tab-content');
-        
-        item.addEventListener('click', ()=>{
-            removeActiveClassForNav()
-            item.classList.add('bg-purple')
-
-            const showContent = document.getElementById(tabsButton);
-            hiddenContentTabs()
-            showContent.classList.remove('hidden')
-        })
+    burgerBtn.addEventListener('click', () => {
+        mobileContainer.classList.toggle('hidden')
+        burgerBtn.classList.toggle('after:bg-purple')
+        burgerBtn.classList.toggle('before:bg-purple')
     })
-}
 
-document.querySelectorAll('.tabs').length ? installTabs() :null;
+    
+    function initPartners() {
+        const breakpoint = window.matchMedia("(max-width:991px)");
+        console.log(breakpoint);
+
+        let partnersSwiper;
+
+        const breakpointChecker = function () {
+            if (breakpoint.matches === true) {
+                if (partnersSwiper !== undefined) partnersSwiper.destroy(true, true);
+                return;
+            } else if (breakpoint.matches === false) {
+                return enableSwiper();
+            }
+        };
+
+        const enableSwiper = function () {
+            partnersSwiper = new Swiper(".swiper", {
+                    slidesPerView: 4,
+                    speed: 3000,
+                    loop: true,
+                    autoplay: {
+                        delay: 0,
+                        disableOnInteraction: false,
+                    },
+            });
+        };
+        breakpoint.addListener(breakpointChecker);
+        breakpointChecker();
+    }
+
+ 
+    const installTabs = () => {
+        const tabsButtons = document.querySelectorAll('[data-tab-content]');
+        const tabContent = document.querySelectorAll('.tab-content')
+
+        const removeActiveClassForNav = () => {
+            tabsButtons.forEach(item => item.classList.remove('bg-purple'))
+        }
+
+        const hiddenContentTabs = () => {
+            tabContent.forEach(item => item.classList.add('hidden'))
+        }
+
+        tabsButtons.forEach((item, index, array) => {
+            const tabsButton = item.getAttribute('data-tab-content');
+
+            item.addEventListener('click', () => {
+                removeActiveClassForNav()
+                item.classList.add('bg-purple')
+
+                const showContent = document.getElementById(tabsButton);
+                hiddenContentTabs()
+                showContent.classList.remove('hidden')
+            })
+        })
+    }
+
+    initPartners()
+    document.querySelectorAll('.tabs').length ? installTabs() : null;
+})
